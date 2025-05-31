1b9e7437cb244f115dfa717877f889ad38643884
Ben Koo <61488202+sebkoo@users.noreply.github.com>
2025-05-31 15:17:22 -0400

    Isolate cases and clear UserDefaults

--- SwiftCommerce/Domain/Service/CartManager.swift
+++ SwiftCommerce/Domain/Service/CartManager.swift
@@ -8,28 +8,24 @@
 import Foundation
 
 final class CartManager: ObservableObject {
-    @Published private(set) var items: [Product] = []
-    private let cartKey = "saved_cart"
-    private let userDefaults: UserDefaults
-
-    init(userDefaults: UserDefaults = .standard) {
-        self.userDefaults = userDefaults
-        loadCart()
+    @Published private(set) var items: [Product] = [] {
+        didSet { saveCart() }
     }
+
+    private let cartKey = "saved_cart"
+
+    init() { loadCart() }
 
     func addToCart(_ product: Product) {
         items.append(product)
-        saveCart()
     }
 
     func clearCart() {
         items.removeAll()
-        saveCart()
     }
 
     func remove(atOffsets offsets: IndexSet) {
         items.remove(atOffsets: offsets)
-        saveCart()
     }
 
     var totalItems: Int {
@@ -41,17 +37,14 @@ final class CartManager: ObservableObject {
     }
 
     private func saveCart() {
-        let encoder = JSONEncoder()
-        if let data = try? encoder.encode(items) {
-            userDefaults.set(data, forKey: cartKey)
-        }
+        guard let encoded = try? JSONEncoder().encode(items) else { return }
+        UserDefaults.standard.set(encoded, forKey: cartKey)
     }
 
     private func loadCart() {
-        let decoder = JSONDecoder()
-        if let data = UserDefaults.standard.data(forKey: cartKey),
-           let savedItems = try? decoder.decode([Product].self, from: data) {
-            items = savedItems
-        }
+        guard let data = UserDefaults.standard.data(forKey: cartKey),
+              let decoded = try? JSONDecoder().decode([Product].self, from: data)
+        else { return }
+        self.items = decoded
     }
 }
