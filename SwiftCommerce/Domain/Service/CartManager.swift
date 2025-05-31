996191a7f73ac7872e0aaeff7c53b6381adf8418
Bonmyeong Koo - vn56eqd <Bonmyeong.Koo@walmart.com>
2025-05-31 14:57:50 -0400

    Add cart persistence using UserDefaults

--- SwiftCommerce/Domain/Service/CartManager.swift
+++ SwiftCommerce/Domain/Service/CartManager.swift
@@ -9,17 +9,27 @@ import Foundation
 
 final class CartManager: ObservableObject {
     @Published private(set) var items: [Product] = []
+    private let cartKey = "saved_cart"
+    private let userDefaults: UserDefaults
+
+    init(userDefaults: UserDefaults = .standard) {
+        self.userDefaults = userDefaults
+        loadCart()
+    }
 
     func addToCart(_ product: Product) {
         items.append(product)
+        saveCart()
     }
 
     func clearCart() {
         items.removeAll()
+        saveCart()
     }
 
     func remove(atOffsets offsets: IndexSet) {
         items.remove(atOffsets: offsets)
+        saveCart()
     }
 
     var totalItems: Int {
@@ -28,5 +38,20 @@ final class CartManager: ObservableObject {
 
     var totalPrice: Double {
         items.reduce(0) { $0 + $1.price }
+    }
+
+    private func saveCart() {
+        let encoder = JSONEncoder()
+        if let data = try? encoder.encode(items) {
+            userDefaults.set(data, forKey: cartKey)
+        }
+    }
+
+    private func loadCart() {
+        let decoder = JSONDecoder()
+        if let data = UserDefaults.standard.data(forKey: cartKey),
+           let savedItems = try? decoder.decode([Product].self, from: data) {
+            items = savedItems
+        }
     }
 }
