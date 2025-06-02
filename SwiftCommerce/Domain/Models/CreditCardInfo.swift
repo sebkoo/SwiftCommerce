91055c47f6b2f69cb246cf3d76e1fe2de456f4d4
Ben Koo <61488202+sebkoo@users.noreply.github.com>
2025-06-01 22:16:26 -0400

    Add card payment with validation and async order

---
+++ SwiftCommerce/Domain/Models/CreditCardInfo.swift
@@ -0,0 +1,14 @@
+//
+//  CreditCardInfo.swift
+//  SwiftCommerce
+//
+//  Created by Ben Koo on 6/1/25.
+//
+
+import Foundation
+
+struct CreditCardInfo {
+    var number: String
+    var expiry: String
+    var cvv: String
+}
